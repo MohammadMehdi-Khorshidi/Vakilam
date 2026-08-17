e6172406bc615f45f20ce86cfc3ff8e01af2779a
AbolfazlRT <abolfazl.rostamtabar@gmail.com>
2026-08-17 12:17:00 +0330

    Update City model relationships

--- app/Models/City.php
+++ app/Models/City.php
@@ -6,22 +6,34 @@ use Illuminate\Database\Eloquent\Model;
 
 class City extends Model
 {
+    /** Attributes that may be mass assigned. */
     protected $fillable = [
         'name',
         'province_id',
     ];
 
-
-    // A city belongs to one province.
+    /** Province that contains this city. */
     public function province()
     {
         return $this->belongsTo(Province::class);
     }
 
-    
-    // A city has many legal requests.
+    /** Client profiles located in this city. */
+    public function clientProfiles()
+    {
+        return $this->hasMany(ClientProfile::class);
+    }
+
+    /** Lawyer service areas configured for this city. */
+    public function lawyerServiceAreas()
+    {
+        return $this->hasMany(LawyerServiceArea::class);
+    }
+
+    /** Legal requests associated with this city. */
     public function legalRequests()
     {
         return $this->hasMany(LegalRequest::class);
     }
+
 }
