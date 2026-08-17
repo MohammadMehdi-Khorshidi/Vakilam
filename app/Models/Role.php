85f1da66a791a3937b7b4a33622622722a4f4434
AbolfazlRT <abolfazl.rostamtabar@gmail.com>
2026-08-17 12:56:14 +0330

    feat: switch Role model to HasUuids trait

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -3,21 +3,30 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 
 class Role extends Model
 {
-    public $incrementing = false;
+    use HasUuids;
 
-    protected $keyType = 'string';
-
+    /** Attributes that may be mass assigned. */
     protected $fillable = [
         'code',
         'name_fa',
     ];
 
-    // A role belongs to many users.
+    /** Pivot records that assign this role to users. */
+    public function assignments()
+    {
+        return $this->hasMany(UserRole::class);
+    }
+
+    /** Users assigned to this role through user_roles. */
     public function users()
     {
-        return $this->belongsToMany(User::class, 'user_roles');
+        return $this->belongsToMany(User::class, 'user_roles')
+            ->using(UserRole::class)
+            ->withPivot(['id', 'granted_at', 'revoked_at']);
     }
+
 }
