5f7647ee3c18496e40eff0b0310bcda2d58f1dcf
AbolfazlRT <abolfazl.rostamtabar@gmail.com>
2026-08-17 12:55:04 +0330

    feat: add cities relation to Province model

--- app/Models/Province.php
+++ app/Models/Province.php
@@ -6,12 +6,30 @@ use Illuminate\Database\Eloquent\Model;
 
 class Province extends Model
 {
+    /** Attributes that may be mass assigned. */
     protected $fillable = [
         'name',
     ];
 
-    
-    // A province has many legal requests.
+    /** Cities that belong to this province. */
+    public function cities()
+    {
+        return $this->hasMany(City::class);
+    }
+
+    /** Client profiles located in this province. */
+    public function clientProfiles()
+    {
+        return $this->hasMany(ClientProfile::class);
+    }
+
+    /** Lawyer service areas configured for this province. */
+    public function lawyerServiceAreas()
+    {
+        return $this->hasMany(LawyerServiceArea::class);
+    }
+
+    /** Legal requests associated with this province. */
     public function legalRequests()
     {
         return $this->hasMany(LegalRequest::class);
