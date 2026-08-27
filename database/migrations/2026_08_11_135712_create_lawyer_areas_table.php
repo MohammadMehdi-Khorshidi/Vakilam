558eaf9c452d3f975d2587663edd1e94e2ec30d8
AbolfazlRT <abolfazl.rostamtabar@gmail.com>
2026-08-27 22:43:25 +0330

    Fix lawyer service areas migration rollback

--- database/migrations/2026_08_11_135712_create_lawyer_areas_table.php
+++ database/migrations/2026_08_11_135712_create_lawyer_areas_table.php
@@ -29,6 +29,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('lawyer_areas');
+        Schema::dropIfExists('lawyer_service_areas');
     }
 };
