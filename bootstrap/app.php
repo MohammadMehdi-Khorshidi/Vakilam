ce933c0971ee81c60d52bd64cff3c54979663f55
AbolfazlRT <abolfazl.rostamtabar@gmail.com>
2026-09-05 21:33:28 +0330

    Fix Sanctum middleware for token based authentication

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,10 +16,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->api(prepend: [
-            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-        ]);
-
+        
         $middleware->alias([
             'verified' => \App\Http\Middleware\EnsureEmailIsVerified::class,
             'active' => \App\Http\Middleware\EnsureActiveUser::class,
